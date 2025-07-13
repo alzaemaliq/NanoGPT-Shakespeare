4aac23137fa8545627b554e68dd76e4298b252c6
Alzae Maliq <alzaemaliq@gmail.com>
2025-07-13 16:29:51 +0900

    Initial commit

---
+++ train.py
@@ -0,0 +1,256 @@
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 300
+max_steps = 5000
+torch.manual_seed(1337)
+
+
+#Sets the the open TinyShakeSpeare function as f and then assigns it's contents to the variable 'text'.
+with open('TinyShakeSpeare.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+#'Set()' removes all duplicate characters and 'Sorted()' sorts the characters in an ascending order.
+chars=(sorted(set(text)))
+vocab_size=(len(chars))
+
+#Creates a dictionary that maps each unique character to its corresponding index
+plato={ch:i for i, ch in enumerate(chars)}
+#Creates a dictionary that maps each unique index to its corresponding character
+socrates={i:ch for i, ch in enumerate(chars)}
+
+#'s' is the input string it takes in, 'for c in s' loops over each character in the input string, and `plato[c]` converts it to its mapped number.
+encode = lambda s: [plato[c] for c in s]
+decode = lambda l: ''.join(socrates[n] for n in l)
+
+#Converts it into a tensor array (required for most tensor operations).
+data_array = torch.tensor(encode(text), dtype=torch.long)
+
+#Finds the index of value in the array at 90th percent.
+cutoff = (int(0.9*len(data_array)))
+
+#Limits the variable 'train_data' to only contain 90% of the total data_array.
+train_data = data_array[:cutoff]
+#Assigns the leftover of data_array as validation data.
+val_data = data_array[cutoff:]
+
+#Both 'x' and 'y' must be equal value so the below loop doesn't fail.
+block_size = 8
+batch_size = 32
+
+# #'x' are values of data_array from the index 0-7, totalling 8 indexes as defined in block_size.
+# # e.g. [A:0, B:1, C:2, D:3,...]
+# x = train_data[:block_size]
+
+# #'y' offsets x by 1, from index 1-9, totalling an equal 8 indexes.
+# # e.g. [0:A, 1:B, 2:C, 3:D,...]
+# y = train_data[1:block_size+1]
+
+# #Loops 8 times (the total value block_size is 8, so it loops over every value in block_size).
+# for t in range(block_size):
+#     #Using :t means it starts at up until index 0, not including index zero so no value whatsoever. Adding +1 starts it at index 0, the first value.
+#     context = x[:t+1]
+#     #using the index t means it begins in index 0 and as y is offsetted by 1 from x it starts at the second value of x and so forth, keeping it one index ahead of x, which is the target.
+#     target = y[t]
+#     print(f"Input: {context}, Target: {target}")
+
+def get_batch(split):
+    #Decides if 'data' is 'train_data' or 'val_data' based on the arguments passed through 'get_batch()'.
+    data = train_data if split == 'train' else val_data
+
+    # Picks 4 random starting positions (because batch_size = 4) from the training data.
+    # Each value in randpoints is an index (a position in the data), NOT a value from the data.
+    randpoints = torch.randint(len(data) - block_size, (batch_size,))
+
+    # For each random starting index, grab a sequence of 8 characters (not the index itself, but the values from those positions).
+    # Example: if i = 2, this grabs data[2:10] → 8 character *values* starting at position 2.
+    x = torch.stack([data[i:i + block_size] for i in randpoints])
+
+    # y is just like x, but shifted one character to the right.
+    # It represents the "target" next characters for each input in x.
+    y = torch.stack([data[i + 1: i + block_size + 1] for i in randpoints])
+    
+    # Note: the three above lines are what randomly pick chunks from either train_data or val_data to to train the AI on.
+
+    x, y = x.to(device), y.to(device)
+
+    return x, y
+
+
+# #loops 4 times, and through 'c_batch[b]' in context it goes throuch each line of the tensor once.
+# for b in range(batch_size):
+#     #loops 8 times, and through 'c_batch[:n+1]' in context it goes through each number in each line of the tensor from index 0-7.
+#     for n in range(block_size):
+#         #it uses :n+1 because :n means it up to but not include n so the first loop to result in an empty tensor []. But n+1 would mean until and including index 0.
+#         context = t_batch[b, :n+1]
+#         target = v_batch[b, n]
+
+@torch.no_grad()
+def estimate_loss():
+
+    # Dictionary to store the average losses of train data and val data.
+    out = {}
+    
+    # Set the model to evaulation mode for accurate results.
+    model.eval()
+
+    # Loops twice, once using train data, once using val data.
+    for split in ['train', 'val']:
+
+        # Creates a one dimensional empty tensor filled with zeros, with a length of eval_iters.
+        losses = torch.zeros(eval_iters)
+
+        # Loop as many times as the value of eval_iters.
+        for k in range(eval_iters):
+
+            # Retrieves either train data or val data depending on which loop it is in (check line 96).
+            # Assigns the output returned from get_batch to X, Y.
+            # The out put is the 2D tensor of batch_size x block_size. 
+            # Y is the X offsetted by 1 index forward, so Y is the target for X of the same index.
+            X, Y = get_batch(split)
+
+            # Passes the result of get_batch to the forward function in the bigram model.
+            # It returns a 2D tensor of the data (either train or val) and it's loss compared to the correct target token.
+            logits, loss = model(X, Y)
+
+            # Converts the loss tensor into a normal python number.
+            losses[k] = loss.item()
+        
+        # Returns the average of all the losses in the loop, what every eval_iters is set to, and saves it to the 'out' dictionary.
+        # Either saves the average loss of train data or val data depending on what loop it's in (check line 96).
+        out[split] = losses.mean()
+    
+    # Return the model to training mode so no errors occur.
+    model.train()
+    
+    # Return the dictionary containing the average losses of both train and val data.
+    return out
+
+#Wrap the entire model in a class so that it can contain all of the model settings (it is best practice for pytorch).
+class BigramLanguageModel(nn.Module):
+
+    #__init__ can be thought of as the ingredients/settings of the model. 
+    # Keep in mind all use vocab_size inside the model is not directly calling 'vocab_size' defined at line 11 but serves as a arbritary variable name to store the argument passed in 'model = BigramLanguageModel(vocab_size)'
+    def __init__(self, vocab_size):
+
+        #Needed to tell pytorch not to overwrite its own settings within the model's setting but to initialize its own setting and then run the model's setting.
+        super().__init__()   
+        
+        #Creates a 65 row table, one for each unique character in 'vocab_size' defined in line 11 (encoded), with each row containing the all the possible next character (which is 65 unique characters), resulting in a 65x65 table.
+        self.embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    # Note: the value of idx depends on where is this function is being called and what is being passed. 
+    # You can call this function multiple times, with each output being totally unrelated to the other.
+    def forward(self, idx, targets=None):
+
+        #Takes 't_batch', which is stored in variable idx, as replaces it index it in with its corresponding row.
+        logits = self.embedding_table(idx)
+
+        if targets == None:
+            loss = None
+        else:
+            # Assigns each of the three dimension of the tensor as B (batch size), T (block size), C(all possibly probabilities).
+            B, T, C = logits.shape
+
+            # Flattens a 4 batch, 8 values each, 65 probabilities per value tensor into a 1 batch, 32 values total, 65 probabilities per value tensor.
+            logits = logits.view(B*T, C)
+
+            # Flattens a 4 batch, 8 values each tensor in 1 batch, 32 values total.
+            targets = targets.view(B*T)
+
+            # From all the possibile probabilities after softmaxxing, it extracts the model's confidence on the correct next token to compute the loss.
+            # Note: The way in which the model converts back the probability to the character is because it's stored like the following:
+            # token = [0.7, 0.1, 0.2] so when the model chooses 0.7 it means the next token is likely at index 0. 
+            # It then takes from the list of 65 characters, the character at index 0.
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+
+        # Loops as many times as max_new_tokens is set to, each loop using the token predicted in the last loop to predict the next.
+        for _ in range(max_new_tokens):
+            # 'self(idx)' automatically calls the forward function (a feature of nn.module) passing idx to it.
+            # It returns the logits (a three dimensional tensor consisting of batch, tokens in the batch, and 65 raw probabilities per token) and loss of idx which is then assigned to to variable of the same name respectively.
+            logits, loss = self(idx)
+
+            #It takes only takes the last token from all the batches alongside their 65 raw probability distribution.
+            # : means the same as 0: its just cleaner.
+            # adding : is needed to correctly place -1 in the middle which is where it needs to be to reference the sequence and not the batch or raw probability distribution.
+            logits = logits[:, -1, :]
+            
+            # converts the logits (3 dimensional) into probabilities totalling 1 (100%). 
+            # dim=-1 sets it to the last dimension so you don't have to manually count of many dimensions there are. 
+            # The last dimension is the raw probability distribution. 
+            probs = F.softmax(logits, dim=-1)
+
+            # Randomly picks a number from the probabilty distrubtion, thus selecting the next token.
+            # It is random but weighted by the probabilities. 0.6 has a higher chance to get pick than 0.2 but 0.2 might still get picked.
+            # This is what allows GPT models to give different answers to the same question. Essentially, it creates creativity.
+            idx_next = torch.multinomial(probs, num_samples=1)
+
+            # Adds the new predicted token to idx within the dimension tokens are stored (one down from batch).
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
+    
+#This passes the global 'vocab_size's value to the model's local 'vocab_size.'
+#Note: 'def __init' in the model is triggered here once automatically to setup the model. Any subsequent call to the model won't automatically trigger it again. It's a feature of python.
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+# Uses the AdamW optimizer. This is the part where the model uses gradient descent to learn.
+# model.parameters() passes to the model which weights needed to be updated for lower loss.
+# A learning rate of 1e-3 0.001. So if the gradient (proposed change to weights by AdamW is -0.02, you times it with the learning rate).
+# This allows for smaller steps within gradient space so the model doesnt overshoot and fall into a local minima's.
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+
+for steps in range(max_steps):
+
+    # This checks if it’s time to evaluate the model’s performance.
+    # eval_interval is how often you want to evaluate (e.g. every 300 steps)
+    if steps % eval_iters == 0:
+
+        # Calls the estimate_loss func which returns the dictionary containing the average loss for both train and val data. 
+        # Assigns it to the variable 'losses'.
+        losses = estimate_loss()
+
+        # Prints losses alongside the current step.
+        print(f"step {steps}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # Assigns the first out of get_batch (which is x) and the second output of get_batch (which is y) to the corresponding value of c_batch & t_batch. 
+    # Meaning, c_batch = x & t_batch = y.
+    t_batch, v_batch = get_batch('train')
+    
+    # Passes both the training data and the validation data into the model and expects two outputs: 
+    # 1. A transformed logits (the flattened embedding table)
+    # 2. The loss between the predicted next token and the actual target token.
+    logits, loss = model(t_batch, v_batch)
+
+    # Clears old gradients from the last step.
+    # Gradients are how much the optimizer tells the embedding table to tweak weights by. Stored in model.embedding_table.weight.grad
+    optimizer.zero_grad(set_to_none=True)
+
+    # Calculates the gradients: how much each weight contributes to the current loss.
+    loss.backward()
+
+    # This applies the gradients: it actually updates the model’s weights.
+    optimizer.step()
+    
+    # Takes the average loss outputted by the forward function in the model (a 1-element tensor) and converts it into a plain python number.
+    # print(loss.item())
+
+# Creates the prompt: a tensor of shape (1, 1) containing just the token [[0]]. torch.zeros automatically fills an empty tensor with 0's.
+# dtype=torch.long ensures the input is an integer and not a float.
+# device=device moves it to CPU or GPU depending on if the GPU exists/compatible.
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+
+# max_new_tokens determines the amount of loops in the generate function called within the model, aka how many predictions it'll make.
+# [0] selects the first (and only) token in that input row. If you added an additional input row with its own token, you would need to do [0] and [1] and so on the more rows you add.
+# .tolist() converts the tensor to a regular Python list of token indices.
+# decode converts that list of token indices back into readable characters using the vocab map and prints it.
+# Viva la GPT. Holy shit that took forever.
+print(decode(model.generate(context, max_new_tokens=1000)[0].tolist()))
